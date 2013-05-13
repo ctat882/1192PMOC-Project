b8d2f0741bc00bbac4feb78ebc4a52f6008b744b
Corey Tattam <corey_tattam@hotmail.com>
2013-05-14 03:18:12 +1000

    Updated PuzzleCreator
    Added more functions to create new games from the predefined solutions.
    Added more checking functions

--- Program Folder/src/PuzzleCreator.java
+++ Program Folder/src/PuzzleCreator.java
@@ -1,9 +1,17 @@
+import java.util.Random;
+
 /*
  * Used to create unique puzzles to varying
  * difficulties.
  */
 public class PuzzleCreator {
+	//The magic number of Sudoku
 	private final int SIZE = 9;
+	private final int BLOCK = 3;
+	//The number of defined terminal solutions
+	private final int NUM_GAMES = 2;
+	private final int MAX_SWAPS = 5;
+	private final int RAND_COLS = 2;
 	private final int[][] SOLUTION_1 = {{4,9,2,5,3,8,1,6,7},
 										{1,5,7,9,6,2,3,4,8},
 										{3,6,8,4,1,7,2,5,9},
@@ -13,6 +21,17 @@ public class PuzzleCreator {
 										{8,4,3,2,9,5,7,1,6},
 										{2,1,5,6,7,3,8,9,4},
 										{6,7,9,8,4,1,5,3,2}};
+	
+	private final int[][] SOLUTION_2 = {{6,8,2,9,4,7,5,1,3},
+										{3,1,4,6,2,5,7,9,8},
+										{9,7,5,8,3,1,4,6,2},
+										{2,5,7,3,8,6,9,4,1},
+										{1,4,6,7,9,2,3,8,5},
+										{8,9,3,1,5,4,6,2,7},
+										{7,6,9,2,1,3,8,5,4},
+										{4,2,8,5,7,9,1,3,6},
+										{5,3,1,4,6,8,2,7,9}};
+	
 	private Puzzle puzzle;
 	//Default constructor
 	public PuzzleCreator()
@@ -26,12 +45,23 @@ public class PuzzleCreator {
 	//the current puzzles stored in this class
 	public void generateNewPuzzle(Difficulty difficulty)
 	{
-		int[][] newGame = SOLUTION_1;
+		int[][] newGame;
+		// Get a solution from a random number MOD 
+		// the number of predefined solutions
+		Random rand = new Random();		
+		int gameSelect = rand.nextInt(NUM_GAMES);
+		if (gameSelect == 0) {
+			newGame = SOLUTION_1;
+		}
+		else {
+			newGame = SOLUTION_2;
+		}
 		newGame = transformGrid(newGame);
 		//TODO: this if statement is just for debugging
-		if (!checkColumns(newGame)) {
-			System.out.println("Column error");
+		if (!validGame(newGame)) {
+			System.out.println("Error");
 		}
+		
 		else {
 			for (int i = 0; i < 9; i++) {
 				for (int j = 0; j < 9; j++) {
@@ -42,16 +72,84 @@ public class PuzzleCreator {
 		}
 	}
 	
+	
+	/**
+	 * Change the puzzle pattern using the four types of
+	 * propagation.
+	 * @param solution The Solution to transform.
+	 * @return A new random solution.
+	 */
 	private int[][] transformGrid (int[][] solution) {
+		// Create a random number generator
+		Random generator = new Random();		
+		// Propagation 1: The mutual exchange of two digits.
+		int numSwaps = generator.nextInt(MAX_SWAPS);
+		for (int i = 0; i <= numSwaps; i++) {
+			// randomise the numbers to swap
+			swapDigits(solution,generator.nextInt(8) + 1,generator.nextInt(8) + 1);
+		}
+		// Propagation 2: The mutual exchange of two columns in the same block
+		numSwaps = generator.nextInt(3);
+		for (int i = 0; i <= numSwaps; i++) {		
+			swapColumns(solution,generator.nextInt(RAND_COLS) * 3, 
+						generator.nextInt(RAND_COLS),generator.nextInt(RAND_COLS));
+		}
+		// Propagation 3: The mutual exchange of two blocks of columns
+		swapColumnBlocks(solution,generator.nextInt(RAND_COLS)*3,
+							generator.nextInt(RAND_COLS) * 3);
 		
-		swapDigits(solution,1,9);
 		return solution;
+	}
+	/**
+	 * Check that the given grid is a valid Sudoku Game.
+	 * This function checks the three basic rules of
+	 * Sudoku; that there are no duplicates in the rows,
+	 * columns and 3x3 regions.
+	 * @param grid
+	 * @return
+	 */
+	public boolean validGame (int[][] grid) {
+		boolean allValid = true;
+		if (!validRegions(grid)) {
+			allValid = false;
+		}
+		else if (!validColumns(grid)) {
+			allValid = false;
+		}
+		else if (!validRows(grid)) {
+			allValid = false;
+		}
+		return allValid;
+	}
+	/**
+	 * Check that all 3x3 regions have unique values. 
+	 * @param grid
+	 * @return
+	 */
+	private boolean validRegions (int[][] grid) {
+		boolean allValid = true;
+		for (int row = 0; row < 7; row += 3) {
+			for (int col = 0; col < 7; col += 3) {
+				boolean[] valid = new boolean[SIZE];
+				for (int x = 0; x < 3; x++) {
+					for (int y = 0; y < 3; y++) {
+						if (!valid[grid[col + x][row + y] - 1]) {
+							valid[grid[col + x][row + y] - 1] = true;
+						}
+						else { 
+							allValid = false;
+						}
+					}
+				}
+			}			
+		}
+		return allValid;
 	}
 	/**
 	 * Check that all columns have unique values
 	 * @return
 	 */
-	private boolean checkColumns (int[][] grid) {
+	private boolean validColumns (int[][] grid) {
 		boolean allValid = true;
 		for (int col = 0; col < SIZE; col++) {
 			boolean[] valid = new boolean[SIZE];
@@ -67,14 +165,86 @@ public class PuzzleCreator {
 		return allValid;
 	}
 	/**
+	 * Check that all rows have unique values.
+	 * @param grid
+	 * @return
+	 */
+	private boolean validRows (int[][] grid) {
+		boolean allValid = true;
+		for (int row = 0; row < SIZE; row++) {
+			boolean[] valid = new boolean[SIZE];
+			for (int col = 0; col < SIZE; col++) {
+				if (!valid[grid[col][row] - 1]) {
+					valid[grid[col][row] - 1] = true;
+				}
+				else {
+					allValid = false;
+				}
+			}
+		}		
+		return allValid;
+	}
+	/**
+	 * Mutual exchange of two columns in the same block of columns.
+	 * @param grid
+	 * @param colBlock Must be in {0, 3, 6}.
+	 * @param colA Must be in the range of [0, 2].
+	 * @param colB Must be in the range of [0, 2].
+	 */
+	private void swapColumns (int[][] grid, int colBlock, int colA, int colB) {
+		int temp;
+		if (colA != colB) {
+			for (int row = 0; row < SIZE; row++) {
+				temp = grid[colBlock + colA][row];
+				grid[colBlock + colA][row] = grid[colBlock + colB][row];
+				grid[colBlock + colB][row] = temp;				
+			}
+		}
+	}
+	/**
+	 * Mutual exchange of two blocks of columns.
+	 * @param grid
+	 * @param blockA Must be in {0,3,6}.
+	 * @param blockB Must be in {0,3,6}.
+	 */
+	private void swapColumnBlocks (int[][]grid, int blockA, int blockB) {
+		if (blockA != blockB) {
+			// create a temp array 3x9
+			int[][] temp = new int[BLOCK][SIZE];
+			// copy blockA into temp
+			for (int col = blockA; col < blockA + BLOCK; col++) {
+				for (int row = 0; row < SIZE; row++) {
+					temp[col % BLOCK][row] = grid[col][row];
+				}
+			}
+			// blockA = blockB
+			for (int col = 0; col < BLOCK; col++) {
+				for (int row = 0; row < SIZE; row++) {
+					grid[col + blockA][row] = grid[col + blockB][row];
+				}
+			}
+			// blockB = temp
+			for (int col = 0; col < BLOCK; col++) {
+				for (int row = 0; row < SIZE; row++) {
+					grid[col + blockB][row] = temp[col][row];
+				}
+			}
+		}
+		
+	}
+	
+	/**
 	 * Mutual exchange of two digits.
 	 * @param numA
 	 * @param numB
 	 */
 	private void swapDigits (int[][] grid, int numA, int numB) {
+		//TODO: Delete boxIndex - just used for conceptualising 
+		/*
 		int[][] boxIndex = {{0,0},{3,0},{6,0},
 							{0,3},{3,3},{6,3},
 							{0,6},{3,6},{6,6}};
+		*/
 		for (int row = 0; row < 7; row += 3) {
 			for (int col = 0; col < 7; col += 3) {
 				for (int x = 0; x < 3; x++) {
@@ -87,8 +257,7 @@ public class PuzzleCreator {
 						}
 					}
 				}
-			}
-			
+			}			
 		}
 	}
 	//TODO THIS IS FOR DEBUGGING PURPOSES ONLY
